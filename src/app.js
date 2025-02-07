5d380f9478f01b22ba0f1a23e4869320cace868a
Jeyson Hilario <jeyson_hilario@hotmail.com>
2025-02-07 11:39:31 -0300

    added table data view

--- src/app.js
+++ src/app.js
@@ -1,23 +1,36 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { hotspotControlValidation, hotspotControlGetData } from "./controller/controller.js";
+import path from "path";
+import { hotspotControlValidation, hotspotControlGetData, hotspotControlGetIndexHTML } from "./controller/controller.js";
 export const app = express()
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, "public")));
+
 
 // ROTA POST PARA ENVIAR DADOS DO FORMULARIO PARA VALIDAÇÃO
-app.post("/api/validation",async (req, res) => {
+app.post("/api/validation",async ( req, res ) => {
     
   const responseControl = await hotspotControlValidation(req.body);
   res.status(200).json(responseControl);
 
 });
 
-app.get("/api/json",async (req, res) => {
+app.get("/api/json",async ( req, res ) => {
+
   const responseControl = await hotspotControlGetData();
   res.status(200).json(responseControl);
+
+});
+
+app.get("/getUsers", async ( req, res ) => {
+
+  const responseControl = await hotspotControlGetIndexHTML();  
+  res.status(200).send(responseControl);
+
 });
--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,6 +3,7 @@ import { createUserMK } from "../services/create-user-mk.js";
 import { saveData } from "../data/save-db.js";
 import { returnDataBase } from "../data/return-data.js";
 import { returnDate } from "../utils/utils.js";
+import { returnHTML } from "../utils/return-html.js";
 
 export async function hotspotControlValidation(request_body){
   // RECEBE E ENVIA O REQUEST BODY PARA OS SERVICES DE VALIDAÇÃO QUE RETORNA UM OBJETO DE SUCCESS OU ERROR COM SUA MENSAGEM DE STATUS
@@ -29,5 +30,8 @@ export async function hotspotControlValidation(request_body){
 
 export async function hotspotControlGetData(){
   return await returnDataBase();
-  ;
+}
+
+export async function hotspotControlGetIndexHTML(){ 
+  return await returnHTML();
 }
